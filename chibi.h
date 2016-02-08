0c2b2603393798eef25d7f3c5b503ba432cdaaba
Akiba <akiba@ubuntu.(none)>
2016-02-08 12:03:04 +0900

    Added chibiGetChannel() command so that sensniff-freaklabs can display proper channel

--- chibi.h
+++ chibi.h
@@ -65,6 +65,7 @@ uint8_t chibiGetData(uint8_t *data);
 uint8_t chibiGetRSSI();
 uint16_t chibiGetSrcAddr();
 uint8_t chibiSetChannel(uint8_t channel);
+uint8_t chibiGetChannel();
 uint8_t chibiGetPartID();
 void chibiSleepRadio(uint8_t enb);
 void chibiCmdInit(uint32_t speed);
